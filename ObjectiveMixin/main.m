7579e1a822efc030ad3168239195bf55eb547fb9
Vladimir Mitrovic <snipe714@gmail.com>
2011-02-24 10:38:46 +0100

    Initial Commit

---
+++ ObjectiveMixin/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveMixin
+//
+//  Created by Vladimir Mitrovic on 24/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+

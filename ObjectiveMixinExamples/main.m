06f324772d3539bdcd85bcc93744c2df34573bd1
Vladimir Mitrovic <snipe714@gmail.com>
2011-04-14 14:45:51 +0200

    Reorganized file structure a little bit.

--- ObjectiveMixinExamples/main.m
+++ ObjectiveMixinExamples/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  Example
+//  ObjectiveMixinExamples
 //
 //  Created by Vladimir Mitrovic on 14/04/2011.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
@@ -12,7 +12,9 @@
 int main (int argc, const char * argv[]) {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
-	// Test the Singleton mixin
+	// Test the Singleton mixin. This should print out a different object address
+	// for each loop iteration, since we're destroying the instance at the end of
+	// the iteration.
 	for (int i = 0; i < 2; i++) {
 		CandyFactory* candyFactory1 = [CandyFactory instance];
 		CandyFactory* candyFactory2 = [CandyFactory instance];
@@ -20,7 +22,7 @@ int main (int argc, const char * argv[]) {
 		NSLog(@"%@", [candyFactory2 createCandy]);
 		[CandyFactory destroyInstance];
 	}
-
+	
 	[pool drain];
 	return 0;
 }
